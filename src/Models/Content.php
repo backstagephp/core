b0f7efb805004a48405b3d7689d68424a6d7baf4
Mark van Eijk <mark@vaneijk.co>
2024-10-29 09:55:44 +0100

    wip

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -2,10 +2,11 @@
 
 namespace Vormkracht10\Backstage\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Content extends Model
 {
@@ -21,6 +22,11 @@ class Content extends Model
     protected function casts(): array
     {
         return [];
+    }
+
+    public function fields(): MorphToMany
+    {
+        return $this->morphToMany(Field::class, 'fieldable');
     }
 
     public function language(): BelongsTo
