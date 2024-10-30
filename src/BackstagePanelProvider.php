de83c6a76f421b0680e4144ebea4d76c3a87260c
Mark van Eijk <mark@vaneijk.co>
2024-10-30 22:10:09 +0100

    light/dark mode switch for app backgrounds (ios)

--- src/BackstagePanelProvider.php
+++ src/BackstagePanelProvider.php
@@ -2,38 +2,79 @@
 
 namespace Vormkracht10\Backstage;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Support\Facades\Blade;
 use Vormkracht10\Backstage\Models\Site;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Support\Facades\FilamentView;
 use Vormkracht10\Backstage\Pages\Dashboard;
-use Vormkracht10\Backstage\Resources\ContentResource;
-use Vormkracht10\Backstage\Resources\DomainResource;
-use Vormkracht10\Backstage\Resources\FieldResource;
-use Vormkracht10\Backstage\Resources\FormResource;
-use Vormkracht10\Backstage\Resources\LanguageResource;
-use Vormkracht10\Backstage\Resources\MediaResource;
-use Vormkracht10\Backstage\Resources\MenuResource;
-use Vormkracht10\Backstage\Resources\RedirectResource;
-use Vormkracht10\Backstage\Resources\SettingResource;
-use Vormkracht10\Backstage\Resources\SiteResource;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Vormkracht10\Backstage\Resources\TagResource;
+use Vormkracht10\Backstage\Resources\FormResource;
+use Vormkracht10\Backstage\Resources\MenuResource;
+use Vormkracht10\Backstage\Resources\SiteResource;
 use Vormkracht10\Backstage\Resources\TypeResource;
 use Vormkracht10\Backstage\Resources\UserResource;
+use Vormkracht10\Backstage\Resources\FieldResource;
+use Vormkracht10\Backstage\Resources\MediaResource;
+use Vormkracht10\FilamentRedirects\RedirectsPlugin;
+use Vormkracht10\Backstage\Resources\DomainResource;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Vormkracht10\Backstage\Resources\ContentResource;
+use Vormkracht10\Backstage\Resources\SettingResource;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Vormkracht10\Backstage\Resources\LanguageResource;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class BackstagePanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::STYLES_BEFORE,
+            fn(): string => Blade::render(
+                <<<HTML
+                <script>
+                document.addEventListener('livewire:navigated', () => {
+                    if(window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('theme') === 'dark') {
+                        document.getElementsByTagName('html')[0].style.backgroundColor = '#030712';
+
+                        var meta = document.createElement('meta');
+                        meta.setAttribute('name', 'background-color');
+                        meta.setAttribute('content', '#030712');
+                        document.getElementsByTagName('head')[0].appendChild(meta);
+
+                        var meta = document.createElement('meta');
+                        meta.setAttribute('name', 'theme-color');
+                        meta.setAttribute('content', '#030712');
+                        document.getElementsByTagName('head')[0].appendChild(meta);
+                    }
+                    else {
+                        document.getElementsByTagName('html')[0].style.backgroundColor = '#ffffff';
+
+                        var meta = document.createElement('meta');
+                        meta.setAttribute('name', 'background-color');
+                        meta.setAttribute('content', '#ffffff');
+                        document.getElementsByTagName('head')[0].appendChild(meta);
+
+                        var meta = document.createElement('meta');
+                        meta.setAttribute('name', 'theme-color');
+                        meta.setAttribute('content', '#ffffff');
+                        document.getElementsByTagName('head')[0].appendChild(meta);
+                    }
+                });
+                </script>
+                HTML
+            ),
+        );
+
         return $panel
             ->id('backstage')
             ->path('backstage')
@@ -46,7 +87,7 @@ class BackstagePanelProvider extends PanelProvider
             ->unsavedChangesAlerts()
             ->sidebarCollapsibleOnDesktop()
             ->plugins([
-                // BackstagePlugin::make(),
+                RedirectsPlugin::make(),
             ])
             ->resources([
                 ContentResource::class,
