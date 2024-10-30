63c4113727e49eba5af5965fe4bbd3c4e8f11c2f
Mathieu <mathieu@fellicht.nl>
2024-10-30 16:36:03 +0100

    Nullable for config, add type for fields

--- src/Resources/TypeResource.php
+++ src/Resources/TypeResource.php
@@ -4,6 +4,8 @@ namespace Vormkracht10\Backstage\Resources;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Illuminate\Support\Str;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -41,7 +43,12 @@ class TypeResource extends Resource
             ->schema([
                 TextInput::make('name')
                     ->columnSpanFull()
-                    ->required(),
+                    ->required()
+                    ->live(debounce: 250)
+                    ->afterStateUpdated(function (Set $set, ?string $state) {
+                        $set('slug', Str::slug($state));
+                        $set('name_plural', Str::plural($state));
+                    }),
                 TextInput::make('slug')
                     ->columnSpanFull()
                     ->required(),
