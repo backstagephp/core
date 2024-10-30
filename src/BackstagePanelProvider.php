38a2cb8ef0b4aa2cc23720bcbb07428a3b210e22
markvaneijk <markvaneijk@users.noreply.github.com>
2024-10-30 15:13:40 +0000

    Fix styling

--- src/BackstagePanelProvider.php
+++ src/BackstagePanelProvider.php
@@ -23,7 +23,6 @@ use Vormkracht10\Backstage\Resources\FormResource;
 use Vormkracht10\Backstage\Resources\LanguageResource;
 use Vormkracht10\Backstage\Resources\MediaResource;
 use Vormkracht10\Backstage\Resources\MenuResource;
-use Vormkracht10\Backstage\Resources\RedirectResource;
 use Vormkracht10\Backstage\Resources\SettingResource;
 use Vormkracht10\Backstage\Resources\SiteResource;
 use Vormkracht10\Backstage\Resources\TagResource;
