d826f3771257100e78d883d073a56bc6b21a3245
Casmo <Casmo@users.noreply.github.com>
2024-11-01 14:09:59 +0000

    Fix styling

--- database/migrations/create_menu_table.php
+++ database/migrations/create_menu_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('slug')->primary();
 
             $table->string('name');
-            
+
             $table->timestamps();
             $table->softDeletes();
         });
--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -2,7 +2,6 @@
 
 namespace Vormkracht10\Backstage\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
