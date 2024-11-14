583a02d92e17816e25919e33048f9109e97572ae
Baspa <hello@baspa.dev>
2024-11-14 13:10:54 +0100

    wip

--- src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
+++ src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
@@ -2,19 +2,20 @@
 
 namespace Vormkracht10\Backstage\Resources\SettingResource\RelationManagers;
 
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
-use Filament\Forms\Set;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-use Vormkracht10\Backstage\Enums\Field as EnumsField;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
 use Vormkracht10\Backstage\Models\Field;
+use Vormkracht10\Backstage\Enums\Field as EnumsField;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class FieldsRelationManager extends RelationManager
 {
@@ -28,18 +29,16 @@ class FieldsRelationManager extends RelationManager
 
         $className = 'Vormkracht10\\Backstage\\Fields\\' . Str::studly($fieldType);
 
-        if (!class_exists($className) || !method_exists($className, 'getForm')) {
+        if (! class_exists($className) || ! method_exists($className, 'getForm')) {
             return [];
         }
 
         return app($className)->getForm();
     }
 
+ 
     public function form(Form $form): Form
     {
-        $record = $form->getRecord();
-        $fieldType = $record?->field_type ?? $form->getState()['field_type'] ?? null;
-        
         return $form
             ->schema([
                 Grid::make()
@@ -63,26 +62,24 @@ class FieldsRelationManager extends RelationManager
                                     ->preload()
                                     ->label(__('Field Type'))
                                     ->live(debounce: 250)
+                                    ->reactive()
                                     ->options(EnumsField::array())
                                     ->required()
-                                    ->afterStateUpdated(function ($state, Set $set, callable $get) {
-                                        $set('config', []); // Reset configuration
-                                        $this->updateConfigurationSchema($set, $state); // Load new schema based on field type
+                                    ->afterStateUpdated(function ($state, Set $set) {
+                                        if ($state !== $this->getOwnerRecord()?->field_type) {
+                                            $set('config', []);
+                                        }
                                     }),
                             ]),
                         Section::make('Configuration')
                             ->columns(3)
-                            ->schema($this->getFieldTypeFormSchema($fieldType)), // Initially empty
+                            ->schema(fn (Get $get) => $this->getFieldTypeFormSchema(
+                                $get('field_type')
+                            ))
                     ]),
             ]);
     }
 
-    protected function updateConfigurationSchema(Set $set, ?string $fieldType): void
-    {
-        $schema = $this->getFieldTypeFormSchema($fieldType);
-        
-        $set('config', $schema); // Dynamically set the configuration schema
-    }
 
     public function table(Table $table): Table
     {
