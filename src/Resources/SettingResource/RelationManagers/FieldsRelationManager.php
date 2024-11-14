1699b0b1e1c9d61704fd8478f121f0a4561c1edd
Baspa <Baspa@users.noreply.github.com>
2024-11-14 12:11:36 +0000

    Fix styling

--- src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
+++ src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
@@ -2,20 +2,20 @@
 
 namespace Vormkracht10\Backstage\Resources\SettingResource\RelationManagers;
 
-use Filament\Tables;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Forms\Set;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Illuminate\Support\Str;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Section;
-use Illuminate\Database\Eloquent\Model;
-use Filament\Forms\Components\TextInput;
-use Vormkracht10\Backstage\Models\Field;
-use Vormkracht10\Backstage\Enums\Field as EnumsField;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Vormkracht10\Backstage\Enums\Field as EnumsField;
+use Vormkracht10\Backstage\Models\Field;
 
 class FieldsRelationManager extends RelationManager
 {
@@ -36,7 +36,6 @@ class FieldsRelationManager extends RelationManager
         return app($className)->getForm();
     }
 
- 
     public function form(Form $form): Form
     {
         return $form
@@ -75,11 +74,10 @@ class FieldsRelationManager extends RelationManager
                             ->columns(3)
                             ->schema(fn (Get $get) => $this->getFieldTypeFormSchema(
                                 $get('field_type')
-                            ))
+                            )),
                     ]),
             ]);
     }
-
 
     public function table(Table $table): Table
     {
