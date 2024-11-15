c6bb0df56c7b276266a3e38fd992892d20a11282
Casmo <Casmo@users.noreply.github.com>
2024-11-15 08:27:48 +0000

    Fix styling

--- src/Resources/ContentResource/Pages/ListContent.php
+++ src/Resources/ContentResource/Pages/ListContent.php
@@ -4,7 +4,6 @@ namespace Vormkracht10\Backstage\Resources\ContentResource\Pages;
 
 use Filament\Actions;
 use Filament\Actions\CreateAction;
-use Filament\Forms\Form;
 use Filament\Resources\Pages\ListRecords;
 use Vormkracht10\Backstage\Models\Type;
 use Vormkracht10\Backstage\Resources\ContentResource;
@@ -20,7 +19,7 @@ class ListContent extends ListRecords
                 Type::orderBy('name')->get()->map(
                     fn ($type) => CreateAction::make()
                         ->label(__($type->name))
-                        ->url(fn () : string => route('filament.backstage.resources.content.create', ['type' => $type]))
+                        ->url(fn (): string => route('filament.backstage.resources.content.create', ['type' => $type]))
                         ->slideOver()
                         ->modalWidth('6xl')
                 )->toArray()
