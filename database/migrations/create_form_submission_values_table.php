7621606f72c58a94a3b79a361c861b1076e9bdd2
markvaneijk <markvaneijk@users.noreply.github.com>
2024-11-19 22:05:54 +0000

    Fix styling

--- database/migrations/create_form_submission_values_table.php
+++ database/migrations/create_form_submission_values_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('form_submission_values', function (Blueprint $table) {
             $table->ulid()->primary();
 
-
             $table->timestamps();
         });
     }
--- database/migrations/create_form_submissions_table.php
+++ database/migrations/create_form_submissions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('form_submissions', function (Blueprint $table) {
             $table->ulid()->primary();
 
-
             $table->timestamps();
         });
     }
