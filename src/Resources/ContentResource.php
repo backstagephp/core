b38e872dfa1d5b827d399a892c91d11a7dd0f0d5
markvaneijk <markvaneijk@users.noreply.github.com>
2024-11-19 22:37:52 +0000

    Fix styling

--- src/Resources/ContentResource.php
+++ src/Resources/ContentResource.php
@@ -64,34 +64,34 @@ class ContentResource extends Resource
                                         ->prefixIcon('heroicon-o-link')
                                         ->options(Site::orderBy('default', 'desc')->orderBy('name', 'asc')->pluck('name', 'ulid'))
                                         ->default(Site::where('default', true)->first()?->ulid)
-                                        ->visible(fn() => Site::count() > 0)
-                                        ->hidden(fn() => Site::count() === 1),
+                                        ->visible(fn () => Site::count() > 0)
+                                        ->hidden(fn () => Site::count() === 1),
                                     Select::make('country_code')
                                         ->label(__('Country'))
                                         ->columnSpan(4)
                                         ->placeholder(__('Select Country'))
                                         ->prefixIcon('heroicon-o-globe-europe-africa')
-                                        ->options(Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->get()->mapWithKeys(fn($language) => [
+                                        ->options(Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->get()->mapWithKeys(fn ($language) => [
                                             $language->code => '<img src="data:image/svg+xml;base64,' . base64_encode(file_get_contents(base_path('vendor/vormkracht10/backstage/resources/img/flags/' . $language->code . '.svg'))) . '" class="w-5 inline-block relative" style="top: -1px; margin-right: 3px;"> ' . Locale::getDisplayLanguage($language->code, app()->getLocale()),
                                         ])->sort())
                                         ->allowHtml()
                                         ->default(Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->count() === 1 ? Language::whereActive(1)->whereNotNull('country_code')->first()->country_code : null)
-                                        ->visible(fn() => Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->count() > 0)
-                                        ->hidden(fn() => Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->count() === 1),
+                                        ->visible(fn () => Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->count() > 0)
+                                        ->hidden(fn () => Language::whereActive(1)->whereNotNull('country_code')->distinct('country_code')->count() === 1),
                                     Select::make('language_code')
                                         ->label(__('Language'))
                                         ->columnSpan(4)
                                         ->placeholder(__('Select Language'))
                                         ->prefixIcon('heroicon-o-language')
                                         ->options(
-                                            Language::whereActive(1)->get()->mapWithKeys(fn($language) => [
+                                            Language::whereActive(1)->get()->mapWithKeys(fn ($language) => [
                                                 $language->code => '<img src="data:image/svg+xml;base64,' . base64_encode(file_get_contents(base_path('vendor/vormkracht10/backstage/resources/img/flags/' . $language->code . '.svg'))) . '" class="w-5 inline-block relative" style="top: -1px; margin-right: 3px;"> ' . Locale::getDisplayLanguage($language->code, app()->getLocale()),
                                             ])->sort()
                                         )
                                         ->allowHtml()
                                         ->default(Language::whereActive(1)->count() === 1 ? Language::whereActive(1)->first()->code : Language::whereActive(1)->where('default', true)->first()?->code)
-                                        ->visible(fn() => Language::whereActive(1)->count() > 0)
-                                        ->hidden(fn() => Language::whereActive(1)->count() === 1),
+                                        ->visible(fn () => Language::whereActive(1)->count() > 0)
+                                        ->hidden(fn () => Language::whereActive(1)->count() === 1),
                                 ]),
                                 TextInput::make('name')
                                     ->columnSpanFull()
