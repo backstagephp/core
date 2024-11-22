1b5498409cdb5b9a4057ffe00008e0ec541fc5a3
Casmo <Casmo@users.noreply.github.com>
2024-11-22 13:53:06 +0000

    Fix styling

--- src/Resources/ContentResource/RelationManagers/FieldsRelationManager.php
+++ src/Resources/ContentResource/RelationManagers/FieldsRelationManager.php
@@ -4,6 +4,4 @@ namespace Vormkracht10\Backstage\Resources\ContentResource\RelationManagers;
 
 use Vormkracht10\Backstage\Resources\SettingResource\RelationManagers\FieldsRelationManager as RelationManagersFieldsRelationManager;
 
-class FieldsRelationManager extends RelationManagersFieldsRelationManager
-{
-}
+class FieldsRelationManager extends RelationManagersFieldsRelationManager {}
