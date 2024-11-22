118e4ed5fb81b368cb0d57fcb5955eb19f89025a
markvaneijk <markvaneijk@users.noreply.github.com>
2024-11-22 14:22:58 +0000

    Fix styling

--- src/Models/Language.php
+++ src/Models/Language.php
@@ -2,12 +2,12 @@
 
 namespace Vormkracht10\Backstage\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Vormkracht10\Backstage\Scopes\ScopedBySite;
-use Vormkracht10\Backstage\Factories\LanguageFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Reedware\LaravelCompositeRelations\HasCompositeRelations;
+use Vormkracht10\Backstage\Factories\LanguageFactory;
+use Vormkracht10\Backstage\Scopes\ScopedBySite;
 
 class Language extends Model
 {
