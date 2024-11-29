c755dabc0b69c9a2b5ebfb882e019809f92216ba
arduinomaster22 <arduinomaster22@users.noreply.github.com>
2024-11-29 13:13:55 +0000

    Fix styling

--- src/Resources/BlockResource/RelationManagers/FieldsRelationManager.php
+++ src/Resources/BlockResource/RelationManagers/FieldsRelationManager.php
@@ -5,5 +5,3 @@ namespace Vormkracht10\Backstage\Resources\BlockResource\RelationManagers;
 use Vormkracht10\Backstage\Resources\SettingResource\RelationManagers\FieldsRelationManager as RelationManagersFieldsRelationManager;
 
 class FieldsRelationManager extends RelationManagersFieldsRelationManager {}
-{
-}
