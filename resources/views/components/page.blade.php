e2d8e83f533d8aba6775e3733f40c50e474eb0ea
Mark van Eijk <mark@vaneijk.co>
2024-11-29 23:27:58 +0100

    wip

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,3 +1,27 @@
-<div {{ $attributes }}>
-    {{ $slot}}
-</div>
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title>{!! html_entity_decode($title) !!}</title>
+    <meta charset="utf-8">
+    <link rel="canonical" href="{{ url()->current() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="apple-mobile-web-app-title" content="Rocketeers">
+    <meta name="content-type" content="text/html; charset=utf-8">
+    <meta name="generator" content="Rocketeers">
+    <meta name="robots" content="index,follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="author" content="Mark van Eijk">
+    @if(filled($description))
+    <meta name="description" content="{{ $description }}">
+    <meta property="og:description" content="{{ $description }}">
+    @endif
+    @if(filled($keywords))
+    <meta name="keywords" content="{{ $keywords }}">
+    @endif
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::fallback(function (Request $request) {
+    abort_unless($request->content(), 404);
+
+    if (! $request->isMethod('GET')) {
+        return abort(405, 'Method Not Allowed');
+    }
+    
     return $request->content()->response();
 });
