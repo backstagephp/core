66a2ac743cd14bb85e29268718858888f4c64a5c
Mark van Eijk <mark@vaneijk.co>
2024-12-03 22:23:53 +0100

    wip

--- src/Widgets/ContentUpdatesWidget.php
+++ src/Widgets/ContentUpdatesWidget.php
@@ -2,11 +2,12 @@
 
 namespace Vormkracht10\Backstage\Widgets;
 
-use Filament\Facades\Filament;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Widgets\TableWidget as BaseWidget;
+use Filament\Facades\Filament;
+use Filament\Tables\Columns\ImageColumn;
 use Vormkracht10\Backstage\Models\Content;
+use Filament\Widgets\TableWidget as BaseWidget;
 
 class ContentUpdatesWidget extends BaseWidget
 {
@@ -24,11 +25,21 @@ class ContentUpdatesWidget extends BaseWidget
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->label('Name')
-                    ->url(fn (Content $content) => route('filament.backstage.resources.content.edit', ['tenant' => Filament::getTenant(), 'record' => $content])),
+                    ->url(fn(Content $content) => route('filament.backstage.resources.content.edit', ['tenant' => Filament::getTenant(), 'record' => $content])),
+                ImageColumn::make('authors')
+                    ->label('')
+                    ->circular()
+                    ->stacked()
+                    ->ring(2)
+                    ->alignRight()
+                    ->getStateUsing(fn(Content $record) => collect($record->authors)->pluck('avatar_url')->toArray())
+                    ->limit(1)
+                    ->limitedRemainingText()
+                    ->url(fn(Content $content) => route('filament.backstage.resources.content.edit', ['tenant' => Filament::getTenant(), 'record' => $content])),
                 Tables\Columns\TextColumn::make('edited_at')
                     ->since()
                     ->alignRight()
-                    ->url(fn (Content $content) => route('filament.backstage.resources.content.edit', ['tenant' => Filament::getTenant(), 'record' => $content])),
+                    ->url(fn(Content $content) => route('filament.backstage.resources.content.edit', ['tenant' => Filament::getTenant(), 'record' => $content])),
             ])
             ->defaultPaginationPageOption(5);
     }
