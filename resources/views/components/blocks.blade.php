b120620bd9c54b2b2feece254d33d59737f8e073
Mathieu <mathieu@fellicht.nl>
2024-12-06 14:58:52 +0100

    Resolve components

--- resources/views/components/blocks.blade.php
+++ resources/views/components/blocks.blade.php
@@ -1,8 +1,9 @@
 <div {{ $attributes }}>
     @foreach($blocks as $block)
         @php($className = \Vormkracht10\Backstage\Facades\Backstage::resolveComponent($block['type']))
-        {!! Blade::renderComponent(
-            new $className(['_type' => $block['type']] + $block['data'])
-        ) !!}
+        @php($component = $className::resolve($block['data'] + ['_type' => $block['type']]))
+        @if ($component->shouldRender())
+        {!!  \Illuminate\Support\Facades\Blade::renderComponent($component) !!}
+        @endif
     @endforeach
 </div>
--- src/Backstage.php
+++ src/Backstage.php
@@ -11,7 +11,7 @@ class Backstage
         'default' => '\Vormkracht10\Backstage\View\Components\DefaultBlock',
     ];
 
-    private static array $blocks = [];
+    private static array $blockComponents = [];
 
     public static function registerComponent(string $name, string $component = null): void
     {
@@ -37,16 +37,16 @@ class Backstage
 
     public static function resolveComponent($slug)
     {
-        if (self::$blocks[$slug] ?? false) {
-            return self::$blocks[$slug];
+        if (self::$blockComponents[$slug] ?? false) {
+            return self::$blockComponents[$slug];
         }
 
         $block = Block::select('component')->where('slug', $slug)->first();
 
         if (!$block) {
-            return static::$components['default'];
+            return self::$blockComponents[$slug] = static::$components['default'];
         }
 
-        return self::$blocks[$slug] = static::$components[$block->component ?? 'default'];
+        return self::$blockComponents[$slug] = static::$components[$block->component ?? 'default'];
     }
 }
