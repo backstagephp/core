5be0ac92d83e26e1636367908bae9c5be117102d
Mark van Eijk <mark@vaneijk.co>
2024-12-06 16:33:02 +0100

    wip

--- resources/views/components/blocks.blade.php
+++ resources/views/components/blocks.blade.php
@@ -3,7 +3,7 @@
         @php($className = \Vormkracht10\Backstage\Facades\Backstage::resolveComponent($block['type']))
         @php($component = $className::resolve($block['data'] + ['_type' => $block['type']]))
         @if ($component->shouldRender())
-        {!!  \Illuminate\Support\Facades\Blade::renderComponent($component) !!}
+        {!! \Illuminate\Support\Facades\Blade::renderComponent($component) !!}
         @endif
     @endforeach
 </div>
