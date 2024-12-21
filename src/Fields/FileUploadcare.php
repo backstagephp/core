640bb3b3ff50e4a77aab10ec1c9ba062dec0e5e2
Baspa <hello@baspa.dev>
2024-12-21 15:28:56 +0100

    Configurable uploadcare field

--- src/Fields/FileUploadcare.php
+++ src/Fields/FileUploadcare.php
@@ -4,6 +4,7 @@ namespace Vormkracht10\Backstage\Fields;
 
 use Filament\Forms;
 use Vormkracht10\Backstage\Models\Field;
+use Vormkracht10\FileUploadcare\Enums\Style;
 use Vormkracht10\FileUploadcare\Forms\Components\FileUploadcare as Input;
 
 class FileUploadcare extends FieldBase implements FieldInterface
@@ -12,6 +13,9 @@ class FileUploadcare extends FieldBase implements FieldInterface
     {
         return [
             ...parent::getDefaultConfig(),
+            'uploaderStyle' => Style::INLINE->value,
+            'multiple' => false,
+            'imagesOnly' => false,
         ];
     }
 
@@ -19,7 +23,13 @@ class FileUploadcare extends FieldBase implements FieldInterface
     {
         $input = self::applyDefaultSettings(Input::make($name), $field);
 
-        $input = $input->label($field->name ?? self::getDefaultConfig()['label'] ?? null);
+        $input = $input->label($field->name ?? self::getDefaultConfig()['label'] ?? null)
+            ->uploaderStyle(Style::tryFrom($field->config['uploaderStyle'] ?? null) ?? Style::tryFrom(self::getDefaultConfig()['uploaderStyle']))
+            ->multiple($field->config['multiple'] ?? self::getDefaultConfig()['multiple']);
+
+        if ($field->config['imagesOnly'] ?? self::getDefaultConfig()['imagesOnly']) {
+            $input->imagesOnly();
+        }
 
         return $input;
     }
@@ -38,7 +48,20 @@ class FileUploadcare extends FieldBase implements FieldInterface
                         ->label(__('Field specific'))
                         ->schema([
                             Forms\Components\Grid::make(2)->schema([
-                                //
+                                Forms\Components\Toggle::make('config.multiple')
+                                    ->label(__('Multiple'))
+                                    ->inline(false),
+                                Forms\Components\Toggle::make('config.imagesOnly')
+                                    ->label(__('Images only'))
+                                    ->inline(false),
+                                Forms\Components\Select::make('config.uploaderStyle')
+                                    ->label(__('Uploader style'))
+                                    ->options([
+                                        Style::INLINE->value => __('Inline'),
+                                        Style::MINIMAL->value => __('Minimal'),
+                                        Style::REGULAR->value => __('Regular'),
+                                    ])
+                                    ->required(),
                             ]),
                         ]),
                 ])->columnSpanFull(),
