9da68718a3d5a8751ff9256001f1100afb11686b
Baspa <hello@baspa.dev>
2024-12-22 16:31:08 +0100

    Remove field values in settings table after deleting

--- src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
+++ src/Resources/SettingResource/RelationManagers/FieldsRelationManager.php
@@ -57,7 +57,7 @@ class FieldsRelationManager extends RelationManager
                                     ->required()
                                     ->placeholder(__('Name'))
                                     ->live(debounce: 250)
-                                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
 
                                 TextInput::make('slug')
                                     ->readonly(),
@@ -93,10 +93,10 @@ class FieldsRelationManager extends RelationManager
                             ]),
                         Section::make('Configuration')
                             ->columns(3)
-                            ->schema(fn (Get $get) => $this->getFieldTypeFormSchema(
+                            ->schema(fn(Get $get) => $this->getFieldTypeFormSchema(
                                 $get('field_type')
                             ))
-                            ->visible(fn (Get $get) => filled($get('field_type'))),
+                            ->visible(fn(Get $get) => filled($get('field_type'))),
                     ]),
             ]);
     }
@@ -180,35 +180,15 @@ class FieldsRelationManager extends RelationManager
                             'model_key' => $this->ownerRecord->slug,
                         ];
                     })
-                    ->mutateFormDataUsing(function (array $data, Model $record): array {
-                        $oldSlug = $record->slug;
-                        $newSlug = $data['slug'];
-
-                        if ($newSlug === $oldSlug) {
-                            return $data;
-                        }
-
-                        $existingValues = $this->ownerRecord->values;
-
-                        if (isset($existingValues[$oldSlug])) {
-
-                            $existingValues[$newSlug] = $existingValues[$oldSlug];
-                            unset($existingValues[$oldSlug]);
-
-                            $this->ownerRecord->update([
-                                'values' => $existingValues,
-                            ]);
-                        } else {
-                            $existingValues[$newSlug] = null;
-                        }
-
-                        return $data;
-                    })
+                    ->mutateFormDataUsing(fn(array $data, Model $record): array => $this->handleUpdatingSlugs($data, $record))
                     ->after(function (Component $livewire) {
                         $livewire->dispatch('refreshFields');
                     }),
                 Tables\Actions\DeleteAction::make()
-                    ->after(function (Component $livewire) {
+                    ->after(function (Component $livewire, array $data, Model $record, array $arguments) {
+                        $this->ownerRecord->update([
+                            'values' => collect($this->ownerRecord->values)->forget($record->slug)->toArray()
+                        ]);
                         $livewire->dispatch('refreshFields');
                     }),
             ])
@@ -235,5 +215,32 @@ class FieldsRelationManager extends RelationManager
     public static function getPluralModelLabel(): string
     {
         return __('Fields');
+    }
+
+    private function handleUpdatingSlugs(array $data, Model $record): array
+    {
+        $oldSlug = $record->slug;
+        $newSlug = $data['slug'];
+
+        if ($newSlug === $oldSlug) {
+            return $data;
+        }
+
+        $existingValues = $this->ownerRecord->values;
+
+        // Handle slug update in existing values
+        if (isset($existingValues[$oldSlug])) {
+            // Transfer value from old slug to new slug
+            $existingValues[$newSlug] = $existingValues[$oldSlug];
+            unset($existingValues[$oldSlug]);
+
+            $this->ownerRecord->update([
+                'values' => $existingValues,
+            ]);
+        } else {
+            $existingValues[$newSlug] = null;
+        }
+
+        return $data;
     }
 }
