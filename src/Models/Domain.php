c0d2bc551949b813068d28410a90ea8cc387c496
Mathieu <mathieu@fellicht.nl>
2024-12-31 11:01:52 +0100

    Typing

--- src/Models/Domain.php
+++ src/Models/Domain.php
@@ -5,6 +5,7 @@ namespace Vormkracht10\Backstage\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Vormkracht10\Backstage\Shared\HasPackageFactory;
 
 class Domain extends Model
@@ -16,7 +17,7 @@ class Domain extends Model
 
     protected $guarded = [];
 
-    public function languages()
+    public function languages(): BelongsToMany
     {
         return $this->belongsToMany(Language::class, 'domain_language', 'domain_ulid', 'language_code')
             ->withPivot('path');
