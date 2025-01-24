1a962d1617a3a0bb3817996f70b103fedb4d0819
Baspa <hello@baspa.dev>
2025-01-24 10:31:53 +0100

    Apply the new HasFields trait

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -5,28 +5,21 @@ namespace Vormkracht10\Backstage\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Vormkracht10\Backstage\Shared\HasPackageFactory;
-use Vormkracht10\Fields\Models\Field;
+use Vormkracht10\Fields\Concerns\HasFields;
 
 class Setting extends Model
 {
     use HasPackageFactory;
     use HasUlids;
+    use HasFields;
 
     protected $primaryKey = 'ulid';
 
     protected $table = 'settings';
 
     protected $guarded = [];
-
-    protected function casts(): array
-    {
-        return [
-            'values' => 'array',
-        ];
-    }
 
     public function language(): BelongsTo
     {
@@ -46,10 +39,5 @@ class Setting extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function fields(): MorphMany
-    {
-        return $this->morphMany(Field::class, 'model', 'model_type', 'model_key', 'slug');
     }
 }
