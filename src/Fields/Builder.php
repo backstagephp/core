062194a4ba4cfb01710859f59dc9a816112e7990
Baspa <10845460+Baspa@users.noreply.github.com>
2025-01-31 09:58:46 +0000

    Fix styling

--- src/Fields/Builder.php
+++ src/Fields/Builder.php
@@ -3,20 +3,20 @@
 namespace Vormkracht10\Backstage\Fields;
 
 use Filament\Forms;
-use Vormkracht10\Fields\Fields\Base;
-use Vormkracht10\Fields\Fields\Text;
-use Vormkracht10\Fields\Models\Field;
-use Vormkracht10\Fields\Fields\Select;
+use Filament\Forms\Components\Builder as Input;
+use Filament\Forms\Components\Builder\Block as BuilderBlock;
+use Vormkracht10\Backstage\Contracts\FieldContract;
 use Vormkracht10\Backstage\Models\Block;
+use Vormkracht10\Fields\Fields\Base;
 use Vormkracht10\Fields\Fields\Checkbox;
 use Vormkracht10\Fields\Fields\KeyValue;
-use Vormkracht10\Fields\Fields\Textarea;
 use Vormkracht10\Fields\Fields\RichEditor;
-use Vormkracht10\UploadcareField\Uploadcare;
-use Filament\Forms\Components\Builder as Input;
-use Vormkracht10\Backstage\Contracts\FieldContract;
+use Vormkracht10\Fields\Fields\Select;
+use Vormkracht10\Fields\Fields\Text;
+use Vormkracht10\Fields\Fields\Textarea;
+use Vormkracht10\Fields\Models\Field;
 use Vormkracht10\MediaPicker\Components\MediaPicker;
-use Filament\Forms\Components\Builder\Block as BuilderBlock;
+use Vormkracht10\UploadcareField\Uploadcare;
 
 class Builder extends Base implements FieldContract
 {
