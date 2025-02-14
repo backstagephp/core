067088a8d895211007bcc98c752859fafe8e75df
Baspa <hello@baspa.dev>
2025-02-14 13:00:10 +0100

    Same for types

--- src/Resources/TypeResource.php
+++ src/Resources/TypeResource.php
@@ -2,21 +2,22 @@
 
 namespace Backstage\Resources;
 
-use Backstage\Fields\Filament\RelationManagers\FieldsRelationManager;
-use Backstage\Models\Type;
-use Backstage\Resources\TypeResource\Pages;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\ToggleButtons;
-use Filament\Forms\Form;
-use Filament\Forms\Set;
-use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Backstage\Models\Type;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Backstage\Resources\TypeResource\Pages;
+use Filament\Forms\Components\ToggleButtons;
+use Backstage\Fields\Filament\RelationManagers\FieldsRelationManager;
 
 class TypeResource extends Resource
 {
@@ -52,9 +53,18 @@ class TypeResource extends Resource
                         TextInput::make('name')
                             ->required()
                             ->live(debounce: 250)
-                            ->afterStateUpdated(function (Set $set, ?string $state) {
-                                $set('slug', Str::slug($state));
+                            ->afterStateUpdated(function (Set $set, Get $get, ?string $state, ?string $old, ?Type $record) {
                                 $set('name_plural', Str::plural($state));
+
+                                $currentSlug = $get('slug');
+
+                                if ($record && $record->slug) {
+                                    return;
+                                }
+
+                                if (! $currentSlug || $currentSlug === Str::slug($old)) {
+                                    $set('slug', Str::slug($state));
+                                }
                             }),
                         TextInput::make('name_plural')
                             ->required(),
@@ -62,7 +72,7 @@ class TypeResource extends Resource
                             ->required()
                             ->unique(ignoreRecord: true)
                             ->rules([
-                                fn (): \Closure => function (string $attribute, $value, \Closure $fail) {
+                                fn(): \Closure => function (string $attribute, $value, \Closure $fail) {
                                     if (in_array(strtolower($value), ['content', 'advanced', 'default'])) {
                                         $fail(__('This :attribute cannot be used.', ['attribute' => 'slug']));
                                     }
@@ -96,7 +106,7 @@ class TypeResource extends Resource
                 IconColumn::make('icon')
                     ->label('')
                     ->width(1)
-                    ->icon(fn (string $state): string => 'heroicon-o-' . $state),
+                    ->icon(fn(string $state): string => 'heroicon-o-' . $state),
                 TextColumn::make('name')
                     ->searchable()
                     ->sortable(),
