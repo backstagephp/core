d49e0e000ae74bfc70054b8730adcb1630c4f1c4
Mark van Eijk <mark@vaneijk.co>
2025-02-22 09:03:37 +0100

    wip

--- src/Resources/FormSubmissionValueResource/Pages/CreateFormSubmissionValue.php
+++ src/Resources/FormSubmissionValueResource/Pages/CreateFormSubmissionValue.php
@@ -5,7 +5,7 @@ namespace Backstage\Resources\FormSubmissionValueResource\Pages;
 use Backstage\Resources\FormResource;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreateFormAction extends CreateRecord
+class CreateFormSubmissionValue extends CreateRecord
 {
     protected static string $resource = FormResource::class;
 }
--- src/Resources/FormSubmissionValueResource/Pages/ListFormSubmissionValues.php
+++ src/Resources/FormSubmissionValueResource/Pages/ListFormSubmissionValues.php
@@ -6,7 +6,7 @@ use Backstage\Resources\FormResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListFormActions extends ListRecords
+class ListFormSubmissionValues extends ListRecords
 {
     protected static string $resource = FormResource::class;
 
