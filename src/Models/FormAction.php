2f1b2eb006ffda1a396758c9cb0127744472d4aa
Mathieu <mathieu@fellicht.nl>
2025-03-21 14:09:09 +0100

    Check for correct email before mailing

--- src/Models/FormAction.php
+++ src/Models/FormAction.php
@@ -48,7 +48,11 @@ class FormAction extends Model
     {
         switch ($this->type) {
             case 'email':
-                Mail::to($submission->value($this->config['to_email'] ?? null) ?? $this->config['to_email'])
+                $to = $submission->value($this->config['to_email'] ?? null) ?? $this->config['to_email'];
+                if (filter_var($to, FILTER_VALIDATE_EMAIL) === false) {
+                    return;
+                }
+                Mail::to($to)
                     ->send(new FormActionExecute($this, $submission));
 
                 break;
