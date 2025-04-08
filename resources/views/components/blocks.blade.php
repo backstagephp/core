eeffe4266c58134f86cc465d088bf053ab6fbfaf
Yoni van Haarlem <yoni.vh@gmail.com>
2025-04-08 14:30:37 +0200

    Show before/after slots if there are no blocks

--- resources/views/components/blocks.blade.php
+++ resources/views/components/blocks.blade.php
@@ -1,14 +1,14 @@
-@if ($blocks)
-    <div {{ $attributes }}>
-        {{ $before ?? '' }}
-        @foreach($blocks as $block)
+<div {{ $attributes }}>
+    {{ $before ?? '' }}
+    @if ($blocks)
+        @foreach ($blocks as $block)
             @php($className = \Backstage\Facades\Backstage::resolveComponent($block['type']))
             @php($params = \Backstage\Facades\Backstage::mapParams($block))
             @php($component = $className::resolve($params))
             @if ($component->shouldRender())
-            {!! \Illuminate\Support\Facades\Blade::renderComponent($component) !!}
+                {!! \Illuminate\Support\Facades\Blade::renderComponent($component) !!}
             @endif
         @endforeach
-        {{ $after ?? '' }}
-    </div>
-@endif
+    @endif
+    {{ $after ?? '' }}
+</div>
