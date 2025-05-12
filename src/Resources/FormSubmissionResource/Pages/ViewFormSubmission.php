9d83dafac203583dfaddc1154aba99a4819759e0
Baspa <hello@baspa.dev>
2025-05-12 18:54:34 +0200

    Allow the user to add notes to existing form submissions

--- src/Resources/FormSubmissionResource/Pages/ViewFormSubmission.php
+++ src/Resources/FormSubmissionResource/Pages/ViewFormSubmission.php
@@ -2,9 +2,12 @@
 
 namespace Backstage\Resources\FormSubmissionResource\Pages;
 
-use Backstage\Resources\FormSubmissionResource;
-use Filament\Resources\Pages\ViewRecord;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Textarea;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\ViewRecord;
+use Backstage\Resources\FormSubmissionResource;
 
 class ViewFormSubmission extends ViewRecord
 {
@@ -13,5 +16,32 @@ class ViewFormSubmission extends ViewRecord
     public function getRecord(): Model
     {
         return parent::getRecord()->load(['values.field']);
+    }
+
+    public function getHeaderActions(): array
+    {
+        return [
+            Action::make('notes')
+                ->label(__('Add Notes'))
+                ->fillForm([
+                    'notes' => $this->record->notes,
+                ])
+                ->form([
+                    Textarea::make('notes')
+                        ->label(__('Notes'))
+                        ->rows(3)
+                ])
+                ->modalSubmitActionLabel(__('Save'))
+                ->action(function (array $data) {
+                    $this->record->update([
+                        'notes' => $data['notes']
+                    ]);
+
+                    Notification::make()
+                        ->title(__('Notes saved'))
+                        ->success()
+                        ->send();
+                })
+        ];
     }
 }
