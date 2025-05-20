91148ee785d233f4a3e76be055a655055bffe5e2
Baspa <hello@baspa.dev>
2025-05-20 15:43:26 +0200

    wip

--- src/Models/Concerns/HasContentRelations.php
+++ src/Models/Concerns/HasContentRelations.php
@@ -4,12 +4,13 @@ namespace Backstage\Models\Concerns;
 
 use Backstage\Models\Content;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 trait HasContentRelations
 {
-    public function content(): BelongsToMany
+    public function content(): MorphToMany
     {
-        return $this->belongsToMany(Content::class, 'relationables', 'related_ulid', 'relation_ulid')
+        return $this->morphMany(Content::class, 'relationables', 'related_ulid', 'relation_ulid')
             ->where('related_type', $this->getMorphClass())
             ->where('relation_type', 'content');
     }
