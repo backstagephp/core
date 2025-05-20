968569b11177d500bbfaca3443217f05e0a065a5
Baspa <hello@baspa.dev>
2025-05-20 15:44:48 +0200

    wip

--- src/Models/Type.php
+++ src/Models/Type.php
@@ -5,7 +5,6 @@ namespace Backstage\Models;
 use Backstage\Shared\HasPackageFactory;
 use Illuminate\Database\Eloquent\Model;
 use Backstage\Fields\Concerns\HasFields;
-use Backstage\Models\Concerns\HasContentRelations;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Type extends Model
