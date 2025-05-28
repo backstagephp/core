1764fc42fa5e820adf56859d07f56eb2fd8dff5e
Mark van Eijk <mark@vaneijk.co>
2025-05-28 06:08:45 +0200

    Update SitemapController.php

--- src/Http/Controllers/SitemapController.php
+++ src/Http/Controllers/SitemapController.php
@@ -15,8 +15,6 @@ class SitemapController
             ->rememberForever('sitemap-xml', function () {
                 $doc = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
 
-                $site = request()->get('site', Site::first());
-
                 Content::with('site')
                     ->whereNotNull('path')
                     ->where(function ($query) {
