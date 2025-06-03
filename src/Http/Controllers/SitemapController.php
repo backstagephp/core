1acd10fd5a757bc8bb5a32d2972e0cc12f250807
Mathieu <mathieu@fellicht.nl>
2025-06-03 15:24:40 +0200

    Remove tag

--- src/Http/Controllers/SitemapController.php
+++ src/Http/Controllers/SitemapController.php
@@ -10,7 +10,6 @@ class SitemapController
     public function __invoke(Request $request)
     {
         $xml = cache()
-            ->tags(['sitemap'])
             ->rememberForever('sitemap-xml', function () {
                 $doc = new \SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
 
