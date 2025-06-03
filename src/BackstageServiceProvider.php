3c0ca1dc34acf4b76be50116a49ebda86d1f942c
Mathieu <mathieu@fellicht.nl>
2025-06-03 15:18:39 +0200

    Add missing includes

--- src/BackstageServiceProvider.php
+++ src/BackstageServiceProvider.php
@@ -319,6 +319,8 @@ class BackstageServiceProvider extends PackageServiceProvider
         $configContent = "<?php\n\n";
         $configContent .= "use Backstage\Models\Site;\n";
         $configContent .= "use Backstage\CustomFields\Builder;\n";
+        $configContent .= "use Backstage\CustomFields\CheckboxList;\n";
+        $configContent .= "use Backstage\CustomFields\Select;\n";
         $configContent .= "use Backstage\Resources\ContentResource;\n";
 
         // Custom export function to create more readable output
