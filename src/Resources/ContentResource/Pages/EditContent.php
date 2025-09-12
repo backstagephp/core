a2052b1d92a485e9d73d04f947cc690d67588cc9
Mathieu <mathieu@fellicht.nl>
2025-09-12 11:40:58 +0200

    Check for value

--- src/Resources/ContentResource/Pages/EditContent.php
+++ src/Resources/ContentResource/Pages/EditContent.php
@@ -291,7 +291,7 @@ class EditContent extends EditRecord
     {
         $data = $this->mutateBeforeSave($data);
 
-        $this->data['values'] = $data['values'];
+        $this->data['values'] = $data['values'] ?? [];
 
         unset($data['tags']);
         unset($data['values']);
