02d696548e3edb9573dc010631d7ca00f8feded8
Mark van Eijk <mark@vaneijk.co>
2025-10-21 17:44:33 +0200

    Remove unused meta tags from page component
    Removed unnecessary meta tags for apple mobile web app title and author.

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -10,12 +10,10 @@
     <link rel="canonical" href="{{ $content->url }}">
     @endif
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="apple-mobile-web-app-title" content="Backstage">
     <meta name="content-type" content="text/html; charset=utf-8">
     <meta name="generator" content="Backstage">
     <meta name="robots" content="index,follow">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta property="author" content="Mark van Eijk">
     @if (isset($content->meta_tags['description']))
         <meta name="description" content="{{ $content->meta_tags['description'] }}">
         <meta property="og:description" content="{{ $content->meta_tags['description'] }}">
