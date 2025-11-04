84e4287063bd04c6fef90ef4c5db4c96fa9893b4
Baspa <hello@baspa.dev>
2025-11-04 18:09:12 +0100

    wip

--- src/Resources/MediaResource.php
+++ src/Resources/MediaResource.php
@@ -8,6 +8,7 @@ use Filament\Actions\ViewAction;
 use Filament\Actions\DeleteAction;
 use Filament\Schemas\Components\Grid;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
 use Filament\Infolists\Components\ImageEntry;
 use Backstage\Translations\Laravel\Models\Language;
 use Backstage\Media\Resources\MediaResource as Resource;
@@ -19,6 +20,26 @@ class MediaResource extends Resource
         $altTagsFormSchema = self::getAltTagsFormSchema();
 
         return parent::table($table)
+            ->headerActions([
+                Action::make('upload')
+                    ->modalHeading(__('Upload media'))
+                    ->slideOver()
+                    ->schema([
+                        FileUpload::make('media')
+                            ->label(__('Media'))
+                            ->disk('uploadcare')
+                            ->multiple(),
+                    ])
+                    ->action(function (array $data) {
+                        // dd($data);
+                        // foreach ($data['media'] as $file) {
+                        //     $media = Media::create([
+                        //         'url' => $media['url'],
+                        //         'alt_tags' => $media['alt_tags'],
+                        //     ]);
+                        // }
+                    }),
+            ])
             ->recordActions([
                 ...parent::table($table)->getRecordActions(),
                 Action::make('alt-tags')
